cb1378588b9810d9fc05ea88e79d9204c23aa7fc
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-23 10:07:26 +0800

    added dependent destroy to dailyworkouttracker

--- app/models/daily_workout_tracker.rb
+++ app/models/daily_workout_tracker.rb
@@ -1,5 +1,5 @@
 class DailyWorkoutTracker < ApplicationRecord
   belongs_to :program_tracker
   belongs_to :daily_workout
-  has_many :exercise_trackers
+  has_many :exercise_trackers, dependent: :destroy
 end
