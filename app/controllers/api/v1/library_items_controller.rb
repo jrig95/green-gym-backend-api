9a2bc75e14262c5d4182ab8b45e97b1e4a342985
Sean <seanalexclark@gmail.com>
2022-07-16 19:03:06 +0800

    added the authenticate user to the library items

--- app/controllers/api/v1/library_items_controller.rb
+++ app/controllers/api/v1/library_items_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::LibraryItemsController < Api::V1::BaseController
   # acts_as_token_authentication_handler_for User
+  before_action :authenticate_user!
   before_action :set_library_item, only: [ :show,:update, :destroy ]
 
   def index
